9071293874f3e2edbe8cbe2cb3b3bcf631ed7e40
Muaz osama <moaz21-01130@student.eelu.edu.eg>
2025-09-10 16:48:14 +0300

    number class

---
+++ lib/models/number_model.dart
@@ -0,0 +1,11 @@
+class NumberModel {
+  final String image;
+  final String en;
+  final String jp;
+
+  const NumberModel({
+    required this.image,
+    required this.en,
+    required this.jp,
+  });
+}
--- lib/views/numbers_view.dart
+++ lib/views/numbers_view.dart
@@ -1,7 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:toku_app/models/number_model.dart';
 
 class NumbersView extends StatelessWidget {
   const NumbersView({super.key});
+  final List<NumberModel> numbers = const [
+    NumberModel(
+      en: 'one',
+      jp: 'ichi',
+      image: 'assets/images/numbers/number_one.png',
+    ),
+    NumberModel(
+      en: 'two',
+      jp: 'ni',
+      image: 'assets/images/numbers/number_two.png',
+    ),
+    NumberModel(
+      en: 'three',
+      jp: 'san',
+      image: 'assets/images/numbers/number_three.png',
+    ),
+    NumberModel(
+      en: 'four',
+      jp: 'shi',
+      image: 'assets/images/numbers/number_four.png',
+    ),
+    NumberModel(
+      en: 'five',
+      jp: 'go',
+      image: 'assets/images/numbers/number_five.png',
+    ),
+    NumberModel(
+      en: 'six',
+      jp: 'roku',
+      image: 'assets/images/numbers/number_six.png',
+    ),
+    NumberModel(
+      en: 'seven',
+      jp: 'shichi',
+      image: 'assets/images/numbers/number_seven.png',
+    ),
+    NumberModel(
+      en: 'eight',
+      jp: 'hachi',
+      image: 'assets/images/numbers/number_eight.png',
+    ),
+    NumberModel(
+      en: 'nine',
+      jp: 'kyuu',
+      image: 'assets/images/numbers/number_nine.png',
+    ),
+    NumberModel(
+      en: 'ten',
+      jp: 'juu',
+      image: 'assets/images/numbers/number_ten.png',
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -12,19 +65,29 @@ class NumbersView extends StatelessWidget {
         leading: Icon(Icons.arrow_back, color: Colors.white),
       ),
       body: Column(
-        children: [
-          NumberItem(),
-          NumberItem(),
-          NumberItem(),
-          NumberItem(),
-        ],
+        children: List.generate(
+          numbers.length,
+          (index) => NumberItem(
+            image: numbers[index].image,
+            en: numbers[index].en,
+            jp: numbers[index].jp,
+          ),
+        ),
       ),
     );
   }
 }
 
 class NumberItem extends StatelessWidget {
-  const NumberItem({super.key});
+  const NumberItem({
+    super.key,
+    required this.image,
+    required this.en,
+    required this.jp,
+  });
+  final String image;
+  final String en;
+  final String jp;
 
   @override
   Widget build(BuildContext context) {
@@ -36,9 +99,7 @@ class NumberItem extends StatelessWidget {
             width: 100,
             height: 100,
             color: Color(0xfffef5da),
-            child: Image.asset(
-              'assets/images/numbers/number_one.png',
-            ),
+            child: Image.asset(image),
           ),
           Expanded(
             child: Row(
@@ -50,14 +111,8 @@ class NumberItem extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.center,
                     mainAxisSize: MainAxisSize.min,
                     children: [
-                      Text(
-                        'ichi',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      Text(
-                        'one',
-                        style: TextStyle(color: Colors.white),
-                      ),
+                      Text(jp, style: TextStyle(color: Colors.white)),
+                      Text(en, style: TextStyle(color: Colors.white)),
                     ],
                   ),
                 ),
